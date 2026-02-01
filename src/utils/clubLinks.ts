b3e74e5ee0d2fbe5ea71fb7119980faef05881ae
Willow2517 <mega.kastov@gmail.com>
2026-02-02 01:57:55 +0300

    fix link

--- src/utils/clubLinks.ts
+++ src/utils/clubLinks.ts
@@ -1,7 +1,7 @@
 // src/lib/clubLinks.ts
 export enum ClubLinks {
   lita = "https://t.me/leramire",
-  olya = "https://t.me/olya_kt",
+  olya = "https://t.me/koooriks",
 }
 
 const LINK_VALUES = Object.values(ClubLinks) as string[]
